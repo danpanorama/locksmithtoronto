5b7c9e2641b516b4a61ea9c39240dd1f24c24d13
danpanorama <dani3morcos@gmail.com>
2023-09-20 12:09:56 +0300

    laptop

--- src/components/btns/Socialbtn.jsx
+++ src/components/btns/Socialbtn.jsx
@@ -4,7 +4,8 @@ import '../../css/btn.css';
 function Socialbtn() {
   return (
     <div className="flexRow center socialBox">
-       <div className="fab fa-facebook flexCol center Icon socialBtn facebook"></div>
+     <a href="https://www.facebook.com/profile.php?id=61551114517836" className="cw">
+       <div className="fab fa-facebook flexCol center Icon socialBtn facebook"></div></a>
        <div className="fab fa-twitter  flexCol center Icon socialBtn twitter"></div>
        <div className="fab fa-youtube  flexCol center Icon socialBtn youtube"></div>
 
